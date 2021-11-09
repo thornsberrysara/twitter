33e8e274bd8f7a570b305edb3ae69f3d4c7f0121
Sara Thornsberry <thornsberrysara@gmail.com>
2021-11-08 21:07:27 -0500

    Generated user authentication

--- lib/twitter_web/router.ex
+++ lib/twitter_web/router.ex
@@ -1,6 +1,8 @@
 defmodule TwitterWeb.Router do
   use TwitterWeb, :router
 
+  import TwitterWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,17 +10,11 @@ defmodule TwitterWeb.Router do
     plug :put_root_layout, {TwitterWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
     plug :accepts, ["json"]
-  end
-
-  scope "/", TwitterWeb do
-    pipe_through :browser
-    resources "/tweets", TweetController
-
-    get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
@@ -52,5 +48,40 @@ defmodule TwitterWeb.Router do
 
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", TwitterWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    get "/users/register", UserRegistrationController, :new
+    post "/users/register", UserRegistrationController, :create
+    get "/users/log_in", UserSessionController, :new
+    post "/users/log_in", UserSessionController, :create
+    get "/users/reset_password", UserResetPasswordController, :new
+    post "/users/reset_password", UserResetPasswordController, :create
+    get "/users/reset_password/:token", UserResetPasswordController, :edit
+    put "/users/reset_password/:token", UserResetPasswordController, :update
+  end
+
+  scope "/", TwitterWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    get "/users/settings", UserSettingsController, :edit
+    put "/users/settings", UserSettingsController, :update
+    get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+    resources "/tweets", TweetController
+  end
+
+  scope "/", TwitterWeb do
+    pipe_through [:browser]
+
+    get "/", PageController, :index
+    delete "/users/log_out", UserSessionController, :delete
+    get "/users/confirm", UserConfirmationController, :new
+    post "/users/confirm", UserConfirmationController, :create
+    get "/users/confirm/:token", UserConfirmationController, :edit
+    post "/users/confirm/:token", UserConfirmationController, :update
   end
 end
